eff69606e55fc4b316b99f26ba04ca7c4704d1ae
Shadowgamer483 <60564162+Shadowgamer483@users.noreply.github.com>
2021-07-02 17:03:58 +0200

    Add files via upload

---
+++ C98.py
@@ -0,0 +1,15 @@
+def numcount ():
+    fileName=input("Enter the file name")
+    numberofwords=0
+    file=open(fileName,"r")
+    for line in file:
+        words=line.split()
+        numberofwords=numberofwords+len(words)
+    print(numberofwords)
+numcount()
+
+
+
+
+
+
